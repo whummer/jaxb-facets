2cd8464bd53c1c01107b1767b9192d5b6c88b0a4
Jason Pell <jason@pellcorp.com>
2012-11-27 21:57:03 +1100

    moved the schemavalidation test stuff to jaxb-impl

--- jaxb-impl/src/test/java/at/ac/tuwien/infosys/jaxb/PersonService.java
+++ jaxb-impl/src/test/java/at/ac/tuwien/infosys/jaxb/PersonService.java
@@ -4,8 +4,16 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import org.apache.cxf.annotations.SchemaValidation;
+import org.apache.cxf.annotations.SchemaValidation.SchemaValidationType;
+
+import at.ac.tuwien.infosys.jaxb.test.TestRequest;
+
 @WebService(name = "PersonService", targetNamespace = "http://com.pellcorp/service/PersonService")
+@SchemaValidation(type = SchemaValidationType.BOTH)
 public interface PersonService {
-    @WebMethod(operationName = "saveInheritEndpoint")
+    @WebMethod(operationName = "save")
     void save(@WebParam(name = "Person") Person data);
+    
+    void foo(@WebParam(name = "TestRequest") TestRequest r);
 }
--- jaxb-impl/src/test/java/at/ac/tuwien/infosys/jaxb/PersonServiceImpl.java
+++ jaxb-impl/src/test/java/at/ac/tuwien/infosys/jaxb/PersonServiceImpl.java
@@ -2,10 +2,15 @@ package at.ac.tuwien.infosys.jaxb;
 
 import javax.jws.WebService;
 
+import at.ac.tuwien.infosys.jaxb.test.TestRequest;
+
 @WebService(endpointInterface = "at.ac.tuwien.infosys.jaxb.PersonService", 
 serviceName = "PersonService", 
 targetNamespace = "http://com.pellcorp/service/PersonService")
 public class PersonServiceImpl implements PersonService {
     public void save(Person data) {
+    }
+
+    public void foo(TestRequest r) {
     }
 }
