acfd4d7c33983e3667a2bb6051b80e2f82dfa026
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2013-02-15 01:27:43 +0100

    minor

--- runTest.sh
+++ runTest.sh
@@ -5,8 +5,8 @@ JAVA7=/usr/lib/jvm/java-7-openjdk-i386/
 
 echo "Running tests with Java 7"
 export JAVA_HOME=$JAVA7
-mvn clean test -Pinternal
+mvn clean install
 
 echo "Running tests with Java 6"
 export JAVA_HOME=$JAVA6
-mvn clean test -Pregular
+mvn clean install
